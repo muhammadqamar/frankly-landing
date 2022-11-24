1a13b03710a7148d87248a6ce8f6738afd6924ae
muhammadqamar <muhammadqamar111@gmail.com>
2022-11-24 12:36:16 +0500

    faq seo

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,10 +1,13 @@
-import React from "react";
+import Head from "next/head";
 import Layout from "../components/layout";
 import FaqPage from "../components/FaqPage";
 
 const Faq = () => {
   return (
     <Layout>
+      <Head>
+        <title>Frankly | Faq </title>
+      </Head>
       <FaqPage />
     </Layout>
   );
